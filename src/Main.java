deb931144f9232dc77acf2265ff967eedd61ce76
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2023-11-16 14:58:01 +0300

    created monster class and created text based game

--- src/Main.java
+++ src/Main.java
@@ -26,5 +26,38 @@ public class Main {
             System.out.println(player1.healthRemaining());
 
          */
+
+        Random rand = new Random();
+
+
+
+        Player player1 = new Player("Johnny", 120, Weapon.DAGGER);
+        System.out.println(player1.toString());
+        Monster monster1 = new Monster("Red Nose Bandit", 120, Weapon.BOW);
+        System.out.println(monster1.toString());
+        int i = 1;
+        while(!(player1.healthRemaining() == 0 || monster1.healthRemaining() == 0)){
+            System.out.println("$$$ Round "+i+" $$$");
+            int randAttackToM = rand.nextInt(50);
+            int randAttackToP = rand.nextInt(50);
+            if(!(monster1.healthRemaining() == 0)){
+                monster1.loseHealth(randAttackToM);
+                System.out.println(player1.getName() + " damaged to monster by " + randAttackToM);
+            }
+            if(!(player1.healthRemaining() == 0)){
+                player1.loseHealth(randAttackToP);
+                System.out.println(monster1.getName() + " damaged to player by "+randAttackToP);
+            }
+
+            System.out.println("Player Health: "+ player1.healthRemaining());
+            System.out.println("Monster Health: "+ monster1.healthRemaining());
+            i++;
+            System.out.println("*************************************************************");
+        }
+        if(player1.healthRemaining() == 0){
+            System.out.println("Winner " + monster1.getName());
+        }else if(monster1.healthRemaining() == 0){
+            System.out.println("Winner " + player1.getName());
+        }
     }
 }
--- src/Monster.java
+++ src/Monster.java
@@ -1,38 +1,34 @@
-public class Player {
-    private final String name;
+public class Monster {
+    private  String name;
     private int healthPercentage;
-    private final Weapon weapon;
+    private  Weapon weapon;
 
     private int checkValue(int value){
         return Math.min(value, 100);
     }
-    public Player(String name, int healthPercentage, Weapon weapon){
+
+    public Monster(String name, int healthPercentage, Weapon weapon){
         this.name = name;
         this.healthPercentage =checkValue(healthPercentage);
         this.weapon = weapon;
     }
-
-    public String getName() {
-        return name;
-    }
-
     public int healthRemaining(){
         return healthPercentage;
+    }
+    public String getName() {
+        return name;
     }
     public void loseHealth(int damage){
         if(healthPercentage > damage){
             healthPercentage -= damage;
         }else {
+            System.out.println(name + " monster has been knocked out of game");
             healthPercentage = 0;
         }
     }
-    public void restoreHealth(int healthPotion){
-        healthPercentage = checkValue(healthPercentage + healthPotion);
-    }
-
     @Override
     public String toString() {
-        return "Player{" +
+        return "Monster{" +
                 "name='" + name + '\'' +
                 ", healthPercentage=" + healthPercentage +
                 ", weapon=" + weapon +
